046653c68720c2bfdcfd0790857d0e025af06448
aziz <a.gharbi@mphase.ca>
2020-11-18 16:05:10 -0500

    update profile

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, task } = require("gulp");
 const minifyInline = require("gulp-minify-inline");
+const htmlmin = require("gulp-htmlmin");
 
-task("minify", function() {
+task("minify", function () {
   return src("index.html")
     .pipe(minifyInline())
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(dest(".."));
 });
