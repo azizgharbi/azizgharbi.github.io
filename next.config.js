9cb3b88f3ec475e36a710e1c403294c4a3945fef
aziz <aziz.gharbi@mavtek.com>
2022-10-30 01:00:57 -0400

    fix path 2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: "/azizgharbi.github.io",
-  assetPrefix: "/azizgharbi.github.io",
+  assetPrefix: "https://azizgharbi.github.io/",
 };
 
 module.exports = nextConfig;
