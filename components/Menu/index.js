e93c82d0d0bfe98a0666f6a9b19a6dbdf118fa21
Aziz <med.aziz.gharbii@gmail.com>
2023-02-05 15:20:54 -0500

    feat: add linkedIn link to the menu

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -8,8 +8,19 @@ export default function Menu() {
           </a>
         </li>
         <li>
-          <a href="https://stackoverflow.com/users/7253451/g-aziz">
+          <a
+            href="https://stackoverflow.com/users/7253451/g-aziz"
+            target="_blank"
+          >
             StackOverflow
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/aziz-g-3b6397137/"
+            target="_blank"
+          >
+            Linkedin
           </a>
         </li>
       </ul>
