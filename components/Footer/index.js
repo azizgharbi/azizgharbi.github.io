2b07e07f516dcd4572312c2f75a822e83a1f4e2c
Aziz <med.aziz.gharbii@gmail.com>
2023-03-08 16:14:55 -0500

    feat: correct typo

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="footer">
       <p>
-        The end. <span>build with love!</span>
+        The end. <span>built with Passion!</span>
       </p>
     </div>
   );
