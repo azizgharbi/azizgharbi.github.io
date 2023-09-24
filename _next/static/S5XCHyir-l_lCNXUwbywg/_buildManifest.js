a07f1c1312528cd52d061c4ec719f8d5ba7d20ea
azizgharbi <med.aziz.gharbii@gmail.com>
2023-09-24 04:47:27 +0000

    Deploying to master from @ azizgharbi/azizgharbi.github.io@e27f37576d63d0635a5a4fba208677bf4a3bf718 🚀

--- _next/static/S5XCHyir-l_lCNXUwbywg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/110-a3c06ca8e412f56b.js","static/chunks/pages/index-b8724e7f0915fb4f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
