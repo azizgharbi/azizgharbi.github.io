4a471c80d8ae2887d80daee1ce5de8e7ccf8edd5
azizgharbi <med.aziz.gharbii@gmail.com>
2023-09-26 00:01:56 +0000

    Deploying to master from @ azizgharbi/azizgharbi.github.io@8f1bf9f24353e6c1749e9127c752cc6d22559236 🚀

--- _next/static/9rrL6wM9bClUdOUzgfvrZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/418-2fef75a5f44a19dd.js","static/chunks/pages/index-20ef812d82b3f8ff.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/_fk_bbup2_ZUIqGku00Ny/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/418-2fef75a5f44a19dd.js","static/chunks/pages/index-a0d03a238ff0444e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
