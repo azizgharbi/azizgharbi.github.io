263af9a2b00f4655695366dc7ddbd911c2a65731
flava <med.aziz.gharbii@gmail.com>
2026-01-02 01:57:31 -0500

    feat: update style

--- components/Terminal/index.js
+++ components/Terminal/index.js
@@ -4,8 +4,7 @@ const text = `Aziz Gharbi — Software & Cloud Developer.
 Experienced in Lua, Python, TypeScript/JavaScript, and Linux-based environments.
 AWS architecture design, covering EC2, Lambda, S3, API Gateway, CloudFormation, and containerized deployments (ECS/EKS).
 Focused on scalable backend systems, automation, and DevOps integration, I have experience with monitoring, observability, and system reliability.
-Skilled in CloudWatch, Datadog and centralized logging to ensure performance, uptime, and deep system insight.
-A boatman, steer steady through these digital waves and social dystopia`;
+Skilled in CloudWatch, Datadog and centralized logging to ensure performance, uptime, and deep system insight.`;
 
 export default function Terminal() {
   return (
